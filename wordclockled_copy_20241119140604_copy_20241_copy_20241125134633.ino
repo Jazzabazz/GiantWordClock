a9d886c8bf3f2caecf54b90ed7a1e5f66da4c8d7
Jazzabazz <163893825+Jazzabazz@users.noreply.github.com>
2024-12-18 11:37:52 -0500

    clock with lights

--- wordclockled_copy_20241119140604_copy_20241_copy_20241125134633.ino
+++ wordclockled_copy_20241119140604_copy_20241_copy_20241125134633.ino
@@ -1,10 +1,12 @@
 #include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include "RTClib.h"
 #define PIN 4
 #define NUMPIXELS 100
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 #define DELAYVAL 50
-
-
+RTC_PCF8523 rtc;
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 int hours;    //hours
 int minutes;  //mins
 int z;
@@ -22,7 +24,7 @@ int hourWords[13][2] = {
   { 81, 84 },  // ten
   { 75, 81 },  // eleven
   { 70, 76 },  // twelve
-  { 34, 37 }  // one
+  { 34, 37 }   // one
 };
 
 int minuteWords[12][2] = {
@@ -37,14 +39,58 @@ int minuteWords[12][2] = {
   { 19, 25 },  // twenty
   { 8, 16 },   // a quarter
   { 16, 19 },  // ten (in minutes)
-  { 25, 29 }  // five
+  { 25, 29 }   // five
 };
 
 void setup() {
   pixels.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
+Serial.begin(57600);
+
+#ifndef ESP8266
+  while (!Serial); // wait for serial port to connect. Needed for native USB
+#endif
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  if (! rtc.initialized() || rtc.lostPower()) {
+    Serial.println("RTC is NOT initialized, let's set the time!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  rtc.start();
+  float drift = 43; // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
+  float period_sec = (7 * 86400);  // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
+  float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
+  float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
+  int offset = round(deviation_ppm / drift_unit);
+  Serial.print("Offset is "); Serial.println(offset); // Print to control offset
 }
 
 void loop() {
+      DateTime now = rtc.now();
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    Serial.println();
+    delay(3000);
+    
   pixels.clear();
   // IT IS (below)
   pixels.setPixelColor(0, pixels.Color(50, 0, 50));
@@ -60,9 +106,8 @@ void loop() {
   pixels.setPixelColor(89, pixels.Color(50, 0, 50));
   pixels.show();
 
-
-  z = 12;
-  minutes = 45;
+  z = now.hour();
+  minutes = now.minute();
   int counter = minutes / 5;
   for (int y = minuteWords[counter][0]; y < minuteWords[counter][1]; y++) {
     pixels.setPixelColor(y, pixels.Color(50, 0, 50));
@@ -86,4 +131,5 @@ void loop() {
     }
   }
   pixels.show();
+
 }
