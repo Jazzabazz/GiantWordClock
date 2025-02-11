83d413af023bd892215954aa885e81382879db16
Jazzabazz <163893825+Jazzabazz@users.noreply.github.com>
2025-02-11 14:04:51 -0500

    separated hour and minute functions

--- wordclockled_copy_20241119140604_copy_20241_copy_20241125134633.ino
+++ wordclockled_copy_20241119140604_copy_20241_copy_20241125134633.ino
@@ -6,12 +6,12 @@
 #define DELAYVAL 50
 RTC_PCF8523 rtc;
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+char daysOfTheWeek[7][12] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 int hours;    //hours
 int minutes;  //mins
 int z;
 
-int hourWords[13][2] = {
+int hourWords[12][2] = {
   { 34, 37 },  // one
   { 37, 40 },  // two
   { 40, 45 },  // three
@@ -23,8 +23,7 @@ int hourWords[13][2] = {
   { 66, 70 },  // nine
   { 81, 84 },  // ten
   { 75, 81 },  // eleven
-  { 70, 76 },  // twelve
-  { 34, 37 }   // one
+  { 70, 76 }   // twelve
 };
 
 int minuteWords[12][2] = {
@@ -44,53 +43,55 @@ int minuteWords[12][2] = {
 
 void setup() {
   pixels.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
-Serial.begin(57600);
+  Serial.begin(57600);
 
 #ifndef ESP8266
-  while (!Serial); // wait for serial port to connect. Needed for native USB
+  while (!Serial)
+    ;  // wait for serial port to connect. Needed for native USB
 #endif
 
-  if (! rtc.begin()) {
+  if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
     while (1) delay(10);
   }
 
-  if (! rtc.initialized() || rtc.lostPower()) {
+  if (!rtc.initialized() || rtc.lostPower()) {
     Serial.println("RTC is NOT initialized, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
   rtc.start();
-  float drift = 43; // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
-  float period_sec = (7 * 86400);  // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
-  float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
-  float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
+  float drift = 43;                                      // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
+  float period_sec = (7 * 86400);                        // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
+  float deviation_ppm = (drift / period_sec * 1000000);  //  deviation in parts per million (μs)
+  float drift_unit = 4.34;                               // use with offset mode PCF8523_TwoHours
   int offset = round(deviation_ppm / drift_unit);
-  Serial.print("Offset is "); Serial.println(offset); // Print to control offset
+  Serial.print("Offset is ");
+  Serial.println(offset);  // Print to control offset
 }
 
 void loop() {
-      DateTime now = rtc.now();
+  DateTime now = rtc.now();
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
 
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
 
-    Serial.println();
-    delay(3000);
-    
+  Serial.println();
+  delay(3000);
+
   pixels.clear();
   // IT IS (below)
   pixels.setPixelColor(0, pixels.Color(50, 0, 50));
@@ -108,28 +109,33 @@ void loop() {
 
   z = now.hour();
   minutes = now.minute();
-  int counter = minutes / 5;
+  int counter = 56 / 5;
   for (int y = minuteWords[counter][0]; y < minuteWords[counter][1]; y++) {
     pixels.setPixelColor(y, pixels.Color(50, 0, 50));
     if (counter > 6) {
+      Serial.println("above 6");
       pixels.setPixelColor(32, pixels.Color(50, 0, 50));
       pixels.setPixelColor(33, pixels.Color(50, 0, 50));
-      hours = z;
-      hours = hours + 1;
-      for (int x = hourWords[hours - 1][0]; x < hourWords[hours - 1][1]; x++) {
-        pixels.setPixelColor(x, pixels.Color(50, 0, 50));
-      }
     } else {
+      Serial.println("below 6");
+      Serial.println(counter);
       pixels.setPixelColor(29, pixels.Color(50, 0, 50));
       pixels.setPixelColor(30, pixels.Color(50, 0, 50));
       pixels.setPixelColor(31, pixels.Color(50, 0, 50));
       pixels.setPixelColor(32, pixels.Color(50, 0, 50));
-      hours = z;
-      for (int x = hourWords[hours - 1][0]; x < hourWords[hours - 1][1]; x++) {
-        pixels.setPixelColor(x, pixels.Color(50, 0, 50));
-      }
     }
   }
-  pixels.show();
 
+  z = 2;
+  if (z > 12) z = z - 12;
+  Serial.println(z);
+  Serial.println(counter);
+  hours = z;
+  for (int x = hourWords[hours - 1][0]; x < hourWords[hours - 1][1]; x++) {
+    if (counter > 6) hours = z + 1;
+      pixels.setPixelColor(x, pixels.Color(50, 0, 50));
+    }
+
+  Serial.println(hours);
+  pixels.show();
 }
